4889cfc339ad12faab9d6e593394a3c04ef5d8df
Paul Ross <apaulross@gmail.com>
2023-04-24 11:48:57 +0100

    Pare down requirements.txt. Support Python 3.7 to 3.11. Add build_all.sh

--- tests/unit/test_svf.py
+++ tests/unit/test_svf.py
@@ -201,7 +201,7 @@ def test_SVF_erase(blocks, erase_fpos):
             (
                     ((12, b' '),),
                     0,
-                    'cp_SparseVirtualFile_erase()#338: Can not erase from a SVF. ERROR:'
+                    'cp_SparseVirtualFile_erase()#355: Can not erase from a SVF. ERROR:'
                     ' SparseVirtualFile::erase(): Non-existent file position 0.',
             ),
 
